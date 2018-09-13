871a7e63ef12937bd6f42f9a1791b5921bd04797
wenming00 <897408187@qq.com>
2018-09-13 17:20:45 +0800

    第二次上传

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,15 @@
 <template>
     <h1>首页</h1>
+    <!-- <router-link></router-link> -->
 </template>
 
 <script>
+    
     export default {
         name: "home"
     }
 </script>
 
 <style scoped>
-
+    
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Router from 'vue-router'
 import Vue from 'vue'
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 Vue.use(Router)
 import home from '../components/home'
@@ -10,22 +12,40 @@ const router = new Router({
     {
       path: '/',
       name: 'home',
-      component: home
+      component: home,
+      meta: {
+        title: '首页'
+      }
     },
     {
       path: '/about/:id',
       name: 'about',
-      component: ()=> import('../components/about')
+      component: ()=> import('../components/about'),
+      meta: {
+        title: '详情页'
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: ()=> import('../components/login')
+      component: ()=> import('../components/login'),
+      meta: {
+        title: '登录页'
+      }
     }
  
   ]
 })
-
+router.beforeEach((to,from,next)=>{
+  Nprogress.start()
+  if(to.mata&&to.mata.title){
+    document.title = to.meta.title
+  }
+  next()
+})
+router.afterEach((to,from) => {
+  Nprogress.done()
+})
 export default router
 
 
